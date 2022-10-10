bb05b2bc3e3af5981e15f30790910a9d29426516
kailash <dhunganak1920@gmail.com>
2022-10-10 21:04:15 +0545

    add responsive

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Input from './components/Input';
 import Output from './components/Output';
 function App(){
 return (
-    <>
+    <div className='wrapper'>
 <h1 className="main-heading">Message Encrypter</h1><br/><br/>
 <Input/>
 <Output />
-    </>
+    </div>
 )
 };
 
